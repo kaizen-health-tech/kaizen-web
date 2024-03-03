773480ef0ef0963bf2ea9a206d596ec4fedabb5e
DarkKnight <dk@snehal.ninja>
2024-03-02 20:00:27 -0800

    fixing contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,19 +1,17 @@
 const ContactForm = ({className}) => {
     return (
       <form name="contact" data-netlify="true" method="POST" className={`contact__form shadow p-3 mb-5 bg-body rounded ${className}`}>
-        <input type="hidden" name="form-name" value="contact" />
-        <p className="h3 text-center">GET STARTED</p>
+         <p className="h3 text-center">Leave your contact for more info</p>
         <div className="form-group">
           <input type="text" name="name" placeholder="Your Name" className="form-control" id="name" />
         </div>
         <div className="form-group mt-3">
           <input type="email" name="email" placeholder="Your Email" className="form-control" id="email" />
         </div>
-        <button type="submit" className="btn btn-custom px-5 mt-3">SEND ME MORE INFO</button>
-        <p className="base-text-2 text-center mt-2">We won't send spam.</p>
+        <button type="submit" className="btn btn-custom px-5 mt-3">Submit</button>
+        <p className="base-text-2 text-center mt-2">We will never send spam.</p>
       </form>
     );
   }
-  
+
   export default ContactForm;
-  
