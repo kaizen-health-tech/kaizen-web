457ad025202db0629ee26b368e5a3d1f3d099463
DarkKnight <dk@snehal.ninja>
2024-03-02 23:48:48 -0800

    fix: testing form submission with new form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,17 +1,27 @@
 const ContactForm = ({className}) => {
     return (
-      <form name="contact" data-netlify="true" method="POST" className={`contact__form shadow p-3 mb-5 bg-body rounded ${className}`}>
-         <p className="h3 text-center">Leave your contact for more info</p>
-        <div className="form-group">
-          <input type="text" name="name" placeholder="Your Name" className="form-control" id="name" />
-        </div>
-        <div className="form-group mt-3">
-          <input type="email" name="email" placeholder="Your Email" className="form-control" id="email" />
-        </div>
-        <button type="submit" className="btn btn-custom px-5 mt-3">Submit</button>
-        <p className="base-text-2 text-center mt-2">We will never send spam.</p>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
-    );
-  }
 
-  export default ContactForm;
+    );
+}
+
+export default ContactForm;
